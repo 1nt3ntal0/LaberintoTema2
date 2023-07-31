d332047f49a87d1bbdd29da319b246943861a9ce
1nt3ntal0 <Torresperezomarr@gmail.com>
2023-07-31 00:14:51 -0700

    Omar Torres
    Correccion Script de Audio en objeto de pasillo , Se implementa valor entre para las estrellas , permitiendo generar de diferentes tamaños de 1 - 4 para darle variedad al cielo.

--- Laberinto/Assets/Assets/Musica/viste.cs
+++ Laberinto/Assets/Assets/Musica/viste.cs
@@ -5,20 +5,18 @@ public class viste : MonoBehaviour
     public AudioClip nuevoSonido;
     private bool isInArea;
     private bool hasPlayedNewSound;
+    int Contador=0;
 
     private void OnTriggerEnter(Collider other)
     {
-        int Contador=0;
         if (Contador == 0)
         {
         if (other.CompareTag("Player") && !hasPlayedNewSound)
         {
             isInArea = true;
-            // Obtener el componente AudioSource del objeto "Player"
             AudioSource playerAudioSource = other.GetComponent<AudioSource>();
             if (playerAudioSource != null)
             {
-                // Cambiar el clip del AudioSource del "Player" y reproducir el nuevo sonido
                 playerAudioSource.clip = nuevoSonido;
                 playerAudioSource.Play();
                 hasPlayedNewSound = true;
@@ -33,12 +31,10 @@ public class viste : MonoBehaviour
         if (other.CompareTag("Player") && isInArea)
         {
             isInArea = false;
-            // Obtener el componente AudioSource del objeto "Player"
             AudioSource playerAudioSource = other.GetComponent<AudioSource>();
             if (playerAudioSource != null)
             {
-                // Restaurar el clip original del AudioSource del "Player" y reproducirlo
-                playerAudioSource.clip = null; // Opcionalmente, podr�as volver a asignar el clip original si lo tienes almacenado.
+                playerAudioSource.clip = null; 
                 playerAudioSource.Play();
             }
             hasPlayedNewSound = false;
